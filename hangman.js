b52901b6d551d5c89fd3d764737020a16f63f17c
Lakshmikanth Mhetre <lkthegr8@gmail.com>
2020-10-29 11:58:35 +0530

    video 96 Hangman challenge 2 ( creation of makeGuess function and adding a event listener to window to take input from user )

--- hangman.js
+++ hangman.js
@@ -1,7 +1,7 @@
 const Hangman=function (word, remainingGuesses) {
     this.word=word.toLowerCase().split("")
     this.remainingGuesses=remainingGuesses
-    this.gussedLetters=['c',"l"]
+    this.gussedLetters=[]
 }
 
 Hangman.prototype.getPuzzle=function(){
@@ -18,8 +18,37 @@ Hangman.prototype.getPuzzle=function(){
     return puzzle
 }
 
+Hangman.prototype.makeGuess=function(guess){
+    guess=guess.toLowerCase()
+    const isUnique=!this.gussedLetters.includes(guess)
+    const isBadGuess=!this.word.includes(guess)
+
+    if(isUnique){
+        this.gussedLetters.push(guess)
+    }
+
+    if(isUnique && isBadGuess){
+        this.remainingGuesses--
+    }
+}
+
 const game1=new Hangman("Cat",2)
 console.log(game1.getPuzzle())
+console.log(`remaining guess = ${game1.remainingGuesses}`)
+
 
 const game2=new Hangman("hello",4)
 console.log(game2.getPuzzle())
+console.log(`remaining guess = ${game2.remainingGuesses}`)
+
+
+
+// adding event listener to the window to take input from user
+window.addEventListener("keypress",function(e){
+    const guess=e.key.toLowerCase()
+    game2.makeGuess(guess)
+
+    console.log(game2.getPuzzle())
+    console.log(`remaining guess = ${game2.remainingGuesses}`)
+
+})
