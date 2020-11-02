abd895a0377610348f583a66247c1be924110574
Lakshmikanth Mhetre <lkthegr8@gmail.com>
2020-11-02 08:40:44 +0530

    video 104 http request from javascript ( getting random word from a server )

--- hangman.js
+++ hangman.js
@@ -1,3 +1,5 @@
+
+
 const puzzleElement=document.querySelector("#puzzle")
 const guessElement=document.querySelector("#guesses")
 
@@ -18,3 +20,17 @@ window.addEventListener("keypress",function(e){
     guessElement.textContent=game1.getStatusMessage()
 
 })
+
+
+// making a http request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange',(e)=>{
+    if(e.target.readyState===4){
+        const data=JSON.parse(e.target.responseText)
+        console.log(data)
+    }
+})
+
+request.open("GET","http://puzzle.mead.io/puzzle")
+request.send()
