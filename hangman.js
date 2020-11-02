ab2a0cb779b36f7a91f9e11f83c2d79506086b08
Lakshmikanth Mhetre <lkthegr8@gmail.com>
2020-11-02 10:38:37 +0530

    video 109 call back abstraction challenge ( used two parameter first number of words and other the call back function )

--- hangman.js
+++ hangman.js
@@ -22,7 +22,7 @@ window.addEventListener("keypress",(e) => {
 })
 
 
-getPuzzle((error,puzzle)=>{
+getPuzzle(4,(error,puzzle)=>{
     if(error){
         console.log(`error: ${error}`)
     }else{
@@ -34,20 +34,11 @@ getPuzzle((error,puzzle)=>{
 
 
 
-// this is a challenge not related to the hangman app
-
-// const countryCode = "IN"
-// const countryRequest = new XMLHttpRequest()
-
-// countryRequest.addEventListener('readystatechange',(e)=>{
-//     if (e.target.readyState===4 && e.target.status===200) {
-//         const data=JSON.parse(e.target.responseText)
-//         const country=data.find((country) => country.alpha2Code === countryCode)
-//         console.log(country.name)
-//     } else if (e.target.readyState===4) {
-//         console.log("unable to fetch data")
-//     }
-// })
-
-// countryRequest.open("GET","http://restcountries.eu/rest/v2/all")
-// countryRequest.send()
+// the challenge for callback abstraction
+getCountry("US",(error,country)=>{
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(`country name: ${country.name}`)
+    }
+})
--- requests.js
+++ requests.js
@@ -1,4 +1,4 @@
-const getPuzzle=(callback)=>{
+const getPuzzle=(wordCount,callback)=>{
     // making a http request
 const request = new XMLHttpRequest()
 
@@ -12,6 +12,25 @@ request.addEventListener('readystatechange',(e)=>{
     }
 })
 
-request.open("GET","http://puzzle.mead.io/puzzle?wordCount=1")
+request.open("GET",`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
 request.send()
+}
+
+const getCountry=(countryCode,callback)=>{
+    // this is a challenge not related to the hangman app
+
+    const countryRequest = new XMLHttpRequest()
+
+    countryRequest.addEventListener('readystatechange', (e) => {
+        if (e.target.readyState === 4 && e.target.status === 200) {
+            const data = JSON.parse(e.target.responseText)
+            const country = data.find((country) => country.alpha2Code === countryCode)
+            callback(undefined,country)
+        } else if (e.target.readyState === 4) {
+            callback('unable to fetch data',undefined)
+        }
+    })
+
+    countryRequest.open("GET", "http://restcountries.eu/rest/v2/all")
+    countryRequest.send()
 }
