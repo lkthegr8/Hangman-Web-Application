229a05417ceb58eb101521f29b56ac6fcb89a4fe
Lakshmikanth Mhetre <lkthegr8@gmail.com>
2020-11-02 23:09:38 +0530

    video 119 integrating data into hangman app ( hangman app backend complete )

--- hangman.js
+++ hangman.js
@@ -1,40 +1,44 @@
 const puzzleElement=document.querySelector("#puzzle")
 const guessElement=document.querySelector("#guesses")
 
-const game1=new Hangman("hi cat",4)
-
-
-puzzleElement.textContent=game1.getPuzzle()
-guessElement.textContent=game1.getStatusMessage()
-
+let game1
 
 // adding event listener to the window to take input from user
 window.addEventListener("keypress",(e) => {
     const guess=e.key.toLowerCase()
     game1.makeGuess(guess)
 
+    render()
+
+})
+
+
+// getPuzzle(1).then((puzzle)=>{
+//     console.log(puzzle)
+// },(err)=>{
+//     console.log(`error: ${err}`)
+// })
+
+const render=()=>{
     puzzleElement.textContent=game1.getPuzzle()
-    // getting the status and printing it on the window
     guessElement.textContent=game1.getStatusMessage()
+}
 
-})
+const startGame = async()=>{
+    const puzzle= await getPuzzle(1)
+    game1=new Hangman(puzzle,5)
+    render()
+}
 
+document.querySelector("#reset").addEventListener("click",startGame())
 
-getPuzzle(1).then((puzzle)=>{
-    console.log(puzzle)
-},(err)=>{
-    console.log(`error: ${err}`)
-})
-
-
-
-
+startGame()
 // the challenge for callback abstraction
-getCountry("US").then((country)=>{
-    console.log(`The name of the country: ${country.name}`)
-}).catch((err)=>{
-    console.log(err)
-})
+// getCountry("US").then((country)=>{
+//     console.log(`The name of the country: ${country.name}`)
+// }).catch((err)=>{
+//     console.log(err)
+// })
 
 
 // dummy code for fetch api
@@ -56,13 +60,13 @@ getCountry("US").then((country)=>{
 
 // code for challenge to get location based on ip address
 // and integrating the country api
-getLocation().then((data)=>{
-    console.log(`you live in ${data.city},${data.region},${data.country}`)
-    return data.country
-}).then((country)=>{
-    getCountry(country).then((country)=>{
-        console.log(`The name of the country: ${country.name}`)
-    })
-}).catch((err)=>{
-    console.log(err)
-})
+// getLocation().then((data)=>{
+//     console.log(`you live in ${data.city},${data.region},${data.country}`)
+//     return data.country
+// }).then((country)=>{
+//     getCountry(country).then((country)=>{
+//         console.log(`The name of the country: ${country.name}`)
+//     })
+// }).catch((err)=>{
+//     console.log(err)
+// })
--- requests.js
+++ requests.js
@@ -1,4 +1,15 @@
-const getPuzzle = (wordCount) => {
+const getPuzzle = async (wordCount) => {
+    const response=await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    if (response.status===200) {
+        const data=await response.json()
+        return data.puzzle
+    } else {
+        throw new Error("not able to get the puzzle")
+    }
+}
+
+
+const getPuzzleOld = (wordCount) => {
     return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`,{}).then((response)=>{
         if (response.status===200) {
 
@@ -11,27 +22,24 @@ const getPuzzle = (wordCount) => {
     })
 }
 
-const getCountry=(countryCode) => {
-    return fetch("http://restcountries.eu/rest/v2/all",{}).then((response)=>{
-        if (response.status===200) {
-            return response.json()
-        }else{
-            throw new Error("unable to fetch the countries")
-        }
-    }).then((data)=>{
+const getCountry= async (countryCode) => {
+    const response=await fetch("http://restcountries.eu/rest/v2/all")
+    if (response.status===200) {
+        const data=await response.json()
         return data.find((country) => country.alpha2Code === countryCode)
-    })
+    } else {
+        throw new Error("not able to get country")
+    }
+
 }
 
 
-const getLocation=(countryCode) => {
-    return fetch("http://ipinfo.io/json?token=9ace42026011f5",{}).then((response)=>{
-        if (response.status===200) {
-            return response.json()
-        }else{
-            throw new Error("unable to fetch the location")
-        }
-    }).then((data)=>{
-        return data
-    })
+const getLocation= async () => {
+    const response=await fetch("http://ipinfo.io/json?token=9ace42026011f5")
+    if (response.status===200) {
+        return response.json()
+        } else {
+        throw new Error("not able to get country")
+    }
+    
 }
